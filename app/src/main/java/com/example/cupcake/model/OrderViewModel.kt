521d58dbeae14cfc09ca558e19f93d198919b265
iAhmed3v <devahmdali@gmail.com>
2022-09-07 13:09:27 +0300

    added comments to OrderViewModel.kt

--- app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
+++ app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
@@ -8,25 +8,95 @@ import java.text.NumberFormat
 import java.text.SimpleDateFormat
 import java.util.*
 
+/** Price for a single cupcake */
 private const val PRICE_PER_CUPCAKE = 2.00
+
+/** Additional cost for same day pickup of an order */
 private const val PRICE_FOR_SAME_DAY_PICKUP = 3.00
 
+/**
+ * [OrderViewModel] holds information about a cupcake order in terms of quantity, flavor, and
+ * pickup date. It also knows how to calculate the total price based on these order details.
+ */
 class OrderViewModel : ViewModel() {
 
+    // Quantity of cupcakes in this order
     private val _quantity = MutableLiveData<Int>()
     val quantity: LiveData<Int> = _quantity
 
+    // Cupcake flavor for this order
     private val _flavor = MutableLiveData<String>()
     val flavor: LiveData<String> = _flavor
 
+    // Possible date options
+    val dataOptions = getPickupOptions()
+
+    // Pickup date
     private val _date = MutableLiveData<String>()
     val data: LiveData<String> = _date
 
+    // Price of the order so far
     private val _price = MutableLiveData<Double>()
     val price: LiveData<String> = Transformations.map(_price) {
+        // Format the price into the local currency and return this as LiveData<String>
         NumberFormat.getCurrencyInstance().format(it)
     }
 
+    init {
+        // Set initial values for the order
+        resetOrder()
+    }
+
+
+    /**
+     * Set the quantity of cupcakes for this order.
+     *
+     * @param numberCupcakes to order
+     */
+    fun setQuantity(numberCupcakes: Int) {
+        _quantity.value = numberCupcakes
+        updatePrice()
+    }
+
+    /**
+     * Set the flavor of cupcakes for this order. Only 1 flavor can be selected for the whole order.
+     *
+     * @param desiredFlavor is the cupcake flavor as a string
+     */
+    fun setFlavor(desiredFlavor: String) {
+        _flavor.value = desiredFlavor
+    }
+
+    /**
+     * Set the pickup date for this order.
+     *
+     * @param pickupDate is the date for pickup as a string
+     */
+    fun setData(pickupDate: String) {
+        _date.value = pickupDate
+        updatePrice()
+    }
+
+    /**
+     * Returns true if a flavor has not been selected for the order yet. Returns false otherwise.
+     */
+    fun hasNoFlavorSet(): Boolean {
+        return _flavor.value.isNullOrEmpty()
+    }
+
+    /**
+     * Reset the order by using initial default values for the quantity, flavor, date, and price.
+     */
+    fun resetOrder() {
+        _quantity.value = 0
+        _date.value = dataOptions[0]
+        _flavor.value = ""
+        _price.value = 0.0
+    }
+
+    /**
+     * Updates the price based on the order details.
+     */
     private fun updatePrice() {
         var calculatedPrice = (quantity.value ?: 0) * PRICE_PER_CUPCAKE
 
@@ -37,27 +107,9 @@ class OrderViewModel : ViewModel() {
         _price.value = calculatedPrice
     }
 
-    fun setQuantity(numberCupcakes: Int) {
-        _quantity.value = numberCupcakes
-        updatePrice()
-    }
-
-    fun setFlavor(desiredFlavor: String) {
-        _flavor.value = desiredFlavor
-    }
-
-    fun setData(pickupDate: String) {
-        _date.value = pickupDate
-        updatePrice()
-    }
-
-    fun hasNoFlavorSet(): Boolean {
-        return _flavor.value.isNullOrEmpty()
-    }
-
-    val dataOptions = getPickupOptions()
-
-
+    /**
+     * Returns a list of date options starting with the current date and the following 3 dates.
+     */
     private fun getPickupOptions(): List<String> {
         val options = mutableListOf<String>()
 
@@ -65,7 +117,6 @@ class OrderViewModel : ViewModel() {
 
         val calendar = Calendar.getInstance()
 
-        // Create a list of dates starting with the current date and the following 3 dates
         repeat(4) {
 
             options.add(formatter.format(calendar.time))
@@ -73,16 +124,5 @@ class OrderViewModel : ViewModel() {
         }
 
         return options
-    }
-
-    fun resetOrder() {
-        _quantity.value = 0
-        _date.value = dataOptions[0]
-        _flavor.value = ""
-        _price.value = 0.0
-    }
-
-    init {
-        resetOrder()
     }
 }
